b1589251bd241b53a2f906025342992bf68bc98a
Julia-PC\Julia <music191128@gmail.com>
2023-04-25 01:09:59 +0300

    Изменение на панель с текстами

--- Assets/Marker/TrackedImageInfoMultipleManager.cs
+++ Assets/Marker/TrackedImageInfoMultipleManager.cs
@@ -15,27 +15,27 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
     private TextMeshProUGUI imageTrackedText;
 
     [SerializeField]
-    private GameObject[] arObjectsToPlace;
+    private TextMeshProUGUI[] arObjectsToPlace;
 
-    //[SerializeField]
-    //private Vector3 scaleFactor = new Vector3(0.1f, 0.1f, 0.1f);
+    public GameObject PanelInfo;
 
     private ARTrackedImageManager m_TrackedImageManager;
 
-    private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
+    private Dictionary<string, TextMeshProUGUI> arObjects = new Dictionary<string, TextMeshProUGUI>();
 
     private void Awake()
     {
         m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
 
-        foreach (GameObject arObject in arObjectsToPlace)
+        foreach (var arObject in arObjectsToPlace)
         {
-            GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
-            newARObject.name = arObject.name;
+            //GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
+            TextMeshProUGUI newARObject = arObject;
             arObjects.Add(arObject.name, newARObject);
-            newARObject.SetActive(false);
+            newARObject.enabled = false;
         }
 
+        PanelInfo.SetActive(false);
     }
 
     void OnEnable()
@@ -74,7 +74,9 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
 
             imageTrackedText.text = "Nothing tracked";
 
-            arObject.Value.SetActive(false);
+            arObject.Value.enabled = false;
+
+            PanelInfo.SetActive(false);
         }
 
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
@@ -94,65 +96,19 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
         {
             imageTrackedText.text = trackedImage.referenceImage.name;
 
-            arObjects[trackedImage.referenceImage.name].SetActive(true);
+            PanelInfo.SetActive(true);
+            arObjects[trackedImage.referenceImage.name].enabled = true;
 
             AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
         }
-
-        //imageTrackedText.text = trackedImage.referenceImage.name;
-        /*
-        if (trackedImage.trackingState != TrackingState.None)
-        {
-           
-        }*/
-
-
-
-
-        /*foreach (GameObject go in arObjects.Values)
-        {
-            if (trackedImage.trackingState != TrackingState.None)
-            {
-                go.SetActive(true);
-            }
-            else
-            {
-                go.SetActive(false);
-            }
-        }*/
-
-        /*
-        foreach (GameObject go in arObjects.Values)
-        {
-            //Debug.Log($"Go in arObjects.Values: {go.name}");
-            if (go.name != trackedImage.referenceImage.name)
-            {
-                go.SetActive(false);
-            }
-        }
-        */
-        //Debug.Log($"trackedImage.referenceImage.name: {trackedImage.referenceImage.name}");
     }
 
     void AssignGameObject(string name, Vector3 newPosition)
     {
         if (arObjectsToPlace != null)
         {
-            GameObject goARObject = arObjects[name];
-            //goARObject.SetActive(true);
-            goARObject.transform.position = newPosition;
-            /*goARObject.transform.localScale = scaleFactor;*/
-
-            /*
-            foreach (GameObject go in arObjects.Values)
-            {
-                Debug.Log($"Go in arObjects.Values: {go.name}");
-                if (go.name != name)
-                {
-                    go.SetActive(false);
-                }
-            }
-            */
+            TextMeshProUGUI goARObject = arObjects[name];
+            //goARObject.transform.position = newPosition;
         }
     }
 }
